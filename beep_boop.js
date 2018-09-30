e0920d60eb3e7d601c3f051c99c63e2b17faf61a
astriskit <harry2office@gmail.com>
2018-09-30 09:48:40 +0530

    beep-booping js.

---
+++ beep_boop.js
@@ -0,0 +1,4 @@
+// Make a new directory for your stream - adventure solutions(mkdir stream - adventure and enter it cd./ stream - adventure)
+// Create a new file called beep_boop.js that uses console.log to output "beep boop".
+
+console.log("beep boop");
