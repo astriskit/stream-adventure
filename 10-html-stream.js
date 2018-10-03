9a75965c7b7f33faecd96869bbe6689f313f80be
Hariom Soni <hariom.soni@asticsinc.com>
2018-10-03 19:41:22 +0530

    [WIP] added 10-html-stream.js

---
+++ 10-html-stream.js
@@ -0,0 +1,30 @@
+// Your program will get some html written to stdin.Convert all the inner html to
+// upper -case for elements with a class name of "loud",
+//     and pipe all the html to stdout.
+
+// You can use`trumpet` and`through2` to solve this adventure.
+
+//     With`trumpet` you can create a transform stream from a css selector:
+
+// var trumpet = require('trumpet');
+// var fs = require('fs');
+// var tr = trumpet();
+// fs.createReadStream('input.html').pipe(tr);
+
+// var stream = tr.select('.beep').createStream();
+
+// Now`stream` outputs all the inner html content at`'.beep'` and the data you
+// write to`stream` will appear as the new inner html content.
+
+// Make sure to`npm install trumpet through2` in the directory where your solution
+// file lives.
+
+function htmlStream() {
+  let trumpet = require("trumpet")();
+  let upperCaseStream = require("through2")(function(buf, _, next) {
+    this.push(buf.toString().toUpperCase());
+    next();
+  });
+  trumpet.select(".loop").pipe(upperCaseStream);
+  process.stdin.pipe(trumpet).pipe();
+}
